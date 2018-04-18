5c5861cff4a743855050ee1b3c755344745caf59
lifucheng <2656020596@qq.com>
2018-04-18 14:38:31 +0800

    haha

---
+++ PycharmProjects/sele/wan/wan.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+browser = webdriver.Chrome("/Users/sy/Downloads/chromedriver 2")
+url = 'https://www.zhihu.com/explore'
+browser.get(url)
+logo = browser.find_element_by_id('zh-top-link-logo')
+print(logo)
+print(logo.get_attribute('class'))
